85fdc3e6a71f1022eb3434138a2683c7d51c0fd2
Aaron Kirby <aerk@berkeley.edu>
2021-08-04 13:00:25 -0700

    ok, now ready to turn into a package

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
-GITHUB_URL = 'http://github.com/vfilimonov/co2meter'
+GITHUB_URL = 'https://github.com/Pinkers/offgas'
 
-exec(open('co2meter/_version.py').read())
+exec(open('offgas/_version.py').read())
 
 # Long description to be published in PyPi
 LONG_DESCRIPTION = """
 **offgas** is a Python interface to the USB CO2 monitor with monitoring and
-logging tools for fermentation.
+logging tools for fermentation. Based on co2meter package from Valdimir Fillanov,
+but with updates for offgas measurement in R&D fermentation context.
 """
 
 setup(name='offgas',
@@ -23,11 +24,5 @@ setup(name='offgas',
       install_requires=['hidapi', 'future', 'pandas'],
       include_package_data=True,
       zip_safe=False,
-      entry_points={
-          'console_scripts': ['co2meter_server = co2meter:start_server',
-                              'co2meter_homekit = co2meter:start_homekit',
-                              'co2meter_server_homekit = co2meter:start_server_homekit',
-                              ],
-      },
       classifiers=['Programming Language :: Python :: 3', ]
       )
